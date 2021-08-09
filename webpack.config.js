54c049d7887e2dbaecc11361e6881090e7313810
Newcoder6 Desktop <newcoder6@outlook.com>
2021-08-09 23:21:50 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  // 1
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          options: {
+            modules: true,
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  // 2
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  // 3
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    hot: true,
+  },
+};
